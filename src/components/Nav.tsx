22e229365d1734ea27fe2b0200eb2877d5e33d4a
Muhammad Junaid Farooq <junaidfarooq.pk@gmail.com>
2026-04-12 15:42:50 +0500

    feat: implement admin responsive sidebar and mobile navigation

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,11 @@
 "use client";
 import {
+  Boxes,
+  ClipboardCheck,
   LogOut,
+  Menu,
   Package,
+  PlusCircle,
   Search,
   ShoppingCartIcon,
   User,
@@ -13,6 +17,7 @@ import { useEffect, useRef, useState } from "react";
 import mongoose, { Document } from "mongoose";
 import { AnimatePresence, motion } from "motion/react";
 import { signOut } from "next-auth/react";
+import { createPortal } from "react-dom";
 
 export interface IUser extends Document {
   name: string;
@@ -32,6 +37,8 @@ const Nav = ({ user }: { user: IUser }) => {
 
   const [searchBarOpen, setSearchBarOpen] = useState(false);
   const searchInputRef = useRef<HTMLInputElement>(null);
+
+  const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
     const handelClickOutside = (e: MouseEvent) => {
@@ -54,8 +61,92 @@ const Nav = ({ user }: { user: IUser }) => {
     }
   }, [searchBarOpen]);
 
+  const sideBar = menuOpen
+    ? createPortal(
+        <AnimatePresence>
+          <motion.div
+            initial={{ x: -100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            exit={{ x: -100 }}
+            transition={{ type: "spring", stiffness: 100, damping: 14 }}
+            className="fixed top-0 left-0 h-full w-[75%] sm:w-[60%] z-9999 
+    bg-linear-to-b from-green-800/90 via-green-700/80 to-green-900/90 
+    backdrop-blur-xl border-r border-green-400/20 
+    shadow-[0_0_50px_-10px_rgba(0,255,100,0.3)] 
+    flex flex-col p-6 text-white"
+          >
+            <div className="flex justify-between items-center mb-2">
+              <h1 className="font-extrabold text-2xl tracking-wide text-white/90">
+                Admin Panel
+              </h1>
+              <button
+                className="text-white/80 hover:text-red-400 text-2xl font-bold transition"
+                onClick={() => setMenuOpen(false)}
+              >
+                <X />
+              </button>
+            </div>
+            <div className="flex items-center gap-3 p-3 mt-3 rounded-xl bg-white/10 hover:bg-white/15 transition-all shadow-inner">
+              <div className="relative w-12 h-12 rounded-full overflow-hidden border-2 border-green-400/60 shadow-lg">
+                {user.image ? (
+                  <Image
+                    src={user.image}
+                    alt="user"
+                    fill
+                    className="object-cover rounded-full"
+                  />
+                ) : (
+                  <User />
+                )}
+              </div>
+              <div>
+                <h2 className="text-lg font-semibold text-white">
+                  {user.name}
+                </h2>
+                <p className="text-xs text-green-200 capitalize tracking-wide">
+                  {user.role}
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-col gap-3 font-medium mt-6">
+              <Link
+                href={""}
+                className="flex items-center gap-3 p-3 rounded-lg bg-white/10 hover:bg-white/20 hover:pl-4 transition-all"
+              >
+                <PlusCircle className="w-5 h-5" /> Add Grocery
+              </Link>
+
+              <Link
+                href={""}
+                className="flex items-center gap-3 p-3 rounded-lg bg-white/10 hover:bg-white/20 hover:pl-4 transition-all"
+              >
+                <Boxes className="w-5 h-5" /> View Grocery
+              </Link>
+
+              <Link
+                href={""}
+                className="flex items-center gap-3 p-3 rounded-lg bg-white/10 hover:bg-white/20 hover:pl-4 transition-all"
+              >
+                <ClipboardCheck className="w-5 h-5" /> Manage Orders
+              </Link>
+            </div>
+            <div className="my-5 border-t border-white/20"></div>
+
+            <div
+              className="flex items-center gap-3 text-red-300 font-semibold mt-auto hover:bg-red-500/20 p-3 rounded-lg transition-all cursor-pointer"
+              onClick={async () => await signOut({ callbackUrl: "/" })}
+            >
+              <LogOut className="w-5 h-5 text-red-300" />
+              Logout
+            </div>
+          </motion.div>
+        </AnimatePresence>,
+        document.body,
+      )
+    : null;
+
   return (
-    <div className="w-[95%] fixed top-4 left-1/2 -translate-x-1/2 bg-linear-to-r from-green-500 to-green-700 rounded-2xl shadow-lg shadow-black/30 flex justify-between items-center h-16 px-4 md:px-8 z-50">
+    <div className="w-[98%] fixed top-4 left-1/2 -translate-x-1/2 bg-linear-to-r from-green-500 to-green-700 rounded-2xl shadow-lg shadow-black/30 flex justify-between items-center h-16 px-4 md:px-8 z-50">
       <Link
         href={"/"}
         className="text-white font-extrabold text-2xl sm:text-3xl tracking-wide hover:scale-105 transition-transform"
@@ -63,34 +154,77 @@ const Nav = ({ user }: { user: IUser }) => {
         OmniMart
       </Link>
 
-      <form className="hidden md:flex items-center bg-white rounded-full px-4 py-2 w-1/2 max-w-lg">
-        <Search className="text-gray-500 w-5 h-5 mr-2" />
-        <input
-          type="text"
-          placeholder="Search groceries..."
-          className="w-full outline-none text-gray-700 placeholder-gray-400"
-        />
-      </form>
+      {user.role == "user" && (
+        <form className="hidden md:flex items-center bg-white rounded-full px-4 py-2 w-1/2 max-w-lg">
+          <Search className="text-gray-500 w-5 h-5 mr-2" />
+          <input
+            type="text"
+            placeholder="Search groceries..."
+            className="w-full outline-none text-gray-700 placeholder-gray-400"
+          />
+        </form>
+      )}
 
       <div className="flex items-center gap-3 md:gap-6 relative cursor-pointer">
+        {user.role == "user" && (
+          <>
+            <div
+              onClick={() => setSearchBarOpen((prev) => !prev)}
+              className="bg-white rounded-full w-11 h-11 flex items-center justify-center shadow-md hover:scale-105 transition md:hidden"
+            >
+              <Search className="text-green-600 w-6 h-6" />
+            </div>
+
+            <Link
+              href={""}
+              className="relative bg-white rounded-full w-11 h-11 flex items-center justify-center shadow-md hover:scale-105 transition"
+            >
+              <ShoppingCartIcon className="text-green-600 w-6 h-6" />
+              <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full font-semibold shadow">
+                0
+              </span>
+            </Link>
+          </>
+        )}
+
+        {user.role === "admin" && (
+          <>
+            <div className="hidden md:flex items-center gap-4">
+              <Link
+                href={""}
+                className="flex items-center gap-2 bg-white text-green-700 font-semibold px-4 py-2 rounded-full hover:bg-green-100 transition-all"
+              >
+                <PlusCircle className="w-5 h-5" /> Add Grocery
+              </Link>
+
+              <Link
+                href={""}
+                className="flex items-center gap-2 bg-white text-green-700 font-semibold px-4 py-2 rounded-full hover:bg-green-100 transition-all"
+              >
+                <Boxes className="w-5 h-5" /> View Grocery
+              </Link>
+
+              <Link
+                href={""}
+                className="flex items-center gap-2 bg-white text-green-700 font-semibold px-4 py-2 rounded-full hover:bg-green-100 transition-all"
+              >
+                <ClipboardCheck className="w-5 h-5" /> Manage Orders
+              </Link>
+            </div>
+            <div
+              className="md:hidden bg-white rounded-full w-10 h-10 flex items-center justify-center shadow-md cursor-pointer"
+              onClick={() => setMenuOpen((prev) => !prev)}
+            >
+              <Menu className="text-green-600 w-6 h-6" />
+            </div>
+          </>
+        )}
+
         <div
-          onClick={() => setSearchBarOpen((prev) => !prev)}
-          className="bg-white rounded-full w-11 h-11 flex items-center justify-center shadow-md hover:scale-105 transition md:hidden"
+          ref={profileDropDown}
+          className="relative"
+          onClick={() => setIsOpen((prev) => !prev)}
         >
-          <Search className="text-green-600 w-6 h-6" />
-        </div>
-
-        <Link
-          href={""}
-          className="relative bg-white rounded-full w-11 h-11 flex items-center justify-center shadow-md hover:scale-105 transition"
-        >
-          <ShoppingCartIcon className="text-green-600 w-6 h-6" />
-          <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full font-semibold shadow">
-            0
-          </span>
-        </Link>
-
-        <div ref={profileDropDown} className="relative">
           <div className="relative bg-white rounded-full w-11 h-11 flex items-center justify-center shadow-md overflow-hidden">
             {user.image ? (
               <Image
@@ -137,14 +271,16 @@ const Nav = ({ user }: { user: IUser }) => {
                   </div>
                 </div>
 
-                <Link
-                  href={""}
-                  onClick={() => setIsOpen(false)}
-                  className="flex items-center gap-2 px-3 py-3 hover:bg-green-50 rounded-lg text-gray-700 font-medium"
-                >
-                  <Package className="w-5 h-5 text-gray-600" />
-                  My Orders
-                </Link>
+                {user.role == "user" && (
+                  <Link
+                    href={""}
+                    onClick={() => setIsOpen(false)}
+                    className="flex items-center gap-2 px-3 py-3 hover:bg-green-50 rounded-lg text-gray-700 font-medium"
+                  >
+                    <Package className="w-5 h-5 text-gray-600" />
+                    My Orders
+                  </Link>
+                )}
 
                 <button
                   onClick={() => {
@@ -163,6 +299,7 @@ const Nav = ({ user }: { user: IUser }) => {
           <AnimatePresence>
             {searchBarOpen && (
               <motion.div
+                onClick={(e) => e.stopPropagation()}
                 initial={{ opacity: 0, y: -10, scale: 0.95 }}
                 animate={{ opacity: 1, y: 0, scale: 1 }}
                 transition={{ duration: 0.3 }}
@@ -186,6 +323,7 @@ const Nav = ({ user }: { user: IUser }) => {
           </AnimatePresence>
         </div>
       </div>
+      {sideBar}
     </div>
   );
 };
