59eae09523579dde5dd6d626ff48ddba9f05bab4
Muhammad Junaid Farooq <junaidfarooq.pk@gmail.com>
2026-04-16 21:07:48 +0500

    feat(admin): add manage orders page with admin order card and API

---
+++ src/app/admin/manage-orders/page.tsx
@@ -0,0 +1,51 @@
+"use client";
+import { IOrder } from "@/models/order.model";
+import axios from "axios";
+import { ArrowLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import { motion } from "motion/react";
+import AdminOrderCard from "@/components/AdminOrderCard";
+
+const ManageOrders = () => {
+  const [orders, setOrders] = useState<IOrder[]>();
+  const router = useRouter();
+  useEffect(() => {
+    const getOrders = async () => {
+      try {
+        const result = await axios.get("/api/admin/get-orders");
+        setOrders(result.data);
+        console.log(result);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getOrders();
+  }, []);
+  return (
+    <div className="bg-linear-to-b from-white to-gray-100 min-h-screen w-full">
+      <div className="fixed top-0 left-0 w-full backdrop-blur-lg bg-white/70 shadow-sm border-b z-50">
+        <div className="max-w-3xl mx-auto flex items-center gap-4 px-4 py-3">
+          <button
+            className="p-2 bg-gray-100 rounded-full hover:bg-gray-200 active:scale-95 transition"
+            onClick={() => router.push("/")}
+          >
+            <ArrowLeft size={24} className="text-green-700" />
+          </button>
+          <h1 className="text-xl font-bold text-gray-800">Manage Orders</h1>
+        </div>
+      </div>
+      <div className="max-w-6xl mx-auto px-4 pt-24 pb-16 space-y-8">
+        <div className="space-y-6">
+          {orders?.map((order, index) => (
+            <motion.div key={index}>
+              <AdminOrderCard order={order} />
+            </motion.div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ManageOrders;
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -209,7 +209,7 @@ const Nav = ({ user }: { user: IUser }) => {
               </Link>
 
               <Link
-                href={""}
+                href={"/admin/manage-orders"}
                 className="flex items-center gap-2 bg-white text-green-700 font-semibold px-4 py-2 rounded-full hover:bg-green-100 transition-all"
               >
                 <ClipboardCheck className="w-5 h-5" /> Manage Orders
